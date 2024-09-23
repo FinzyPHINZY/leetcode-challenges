107bec5436731fbf6fe2e865de06657f32b9ae6e
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2024-09-23 10:01:49 +0100

    added new challenge and solution

--- longestCommonPrefix.ts
+++ longestCommonPrefix.ts
@@ -4,19 +4,38 @@
 // longestCommonPrefix(['flower', 'flow', 'flop', flown]) ===> 'flo'
 // longestCommonPrefix(['dog', 'cat', 'race]) ===> ''
 
-const longestCommonPrefix = function (arr: string[]): string {
-  if (arr.length === 0) return ''
+// const longestCommonPrefix = function (arr: string[]): string {
+//   if (arr.length === 0) return ''
+//   let prefix = ''
+
+//   for (let i = 0; i < arr[0].length; i++) {
+//     const char = arr[0][i]
+//     for (let j = 0; j < arr.length; j++) {
+//       if (arr[j][i] !== char) {
+//         return prefix
+//       }
+//     }
+
+//     prefix += char
+//   }
+
+//   return prefix
+// }
+
+const longestCommonPrefix = function (strs: string[]): string {
+  if (strs.length === 0) return ''
+
   let prefix = ''
+  strs = strs.sort()
+  const first = strs[0]
+  const last = strs[strs.length - 1]
 
-  for (let i = 0; i < arr[0].length; i++) {
-    const char = arr[0][i]
-    for (let j = 0; j < arr.length; j++) {
-      if (arr[j][i] !== char) {
-        return prefix
-      }
+  for (let i = 0; i < first.length; i++) {
+    if (first[i] === last[i]) {
+      prefix += first[i]
+    } else {
+      break
     }
-
-    prefix += char
   }
 
   return prefix
