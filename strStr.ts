b719e9568267575c762dbcf4ebdcd5514dc075aa
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2024-09-27 13:15:56 +0100

    added new problem and solution

--- strStr.ts
+++ strStr.ts
@@ -8,3 +8,9 @@ function strStr(haystack: string, needle: string): number {
 
 console.log(strStr('sadbutsad', 'sad'), 0)
 console.log(strStr('leetcode', 'leeto'), -1)
+
+// grades = 0 <= n >=100
+// fail = n < 40
+
+// If the difference between the grade and the next multiple of 5 is less than 3 , round up to the next multiple of 5
+// If the value of grade is less than 38, no rounding occurs as the result will still be a failing grade.
