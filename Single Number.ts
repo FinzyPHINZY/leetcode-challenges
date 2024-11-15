70c9d636127b71ee9a1d7de87084dedea31e26ff
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2024-11-15 14:18:10 +0100

    added problem starter code

--- Single Number.ts
+++ Single Number.ts
@@ -22,3 +22,7 @@
 //     1 <= nums.length <= 3 * 104
 //     -3 * 104 <= nums[i] <= 3 * 104
 //     Each element in the array appears twice except for one element which appears only once.
+
+function singleNumber(nums: number[]): number {
+  return 0;
+}
