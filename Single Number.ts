8fd291efea23193e0dbe7514b348fd4c4c7dc8e6
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2024-11-15 14:25:39 +0100

    added test cases

--- Single Number.ts
+++ Single Number.ts
@@ -26,3 +26,7 @@
 function singleNumber(nums: number[]): number {
   return 0;
 }
+
+console.log(singleNumber([2, 2, 1]));
+console.log(singleNumber([4, 1, 2, 1, 2]));
+console.log(singleNumber([1]));
