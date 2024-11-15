9940a5eb17b1a4be98026e6374e6b58a33127442
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2024-11-15 14:52:13 +0100

    added solutio to problem

--- Majority Element.ts
+++ Majority Element.ts
@@ -21,8 +21,30 @@
 // Follow-up: Could you solve the problem in linear time and in O(1) space?
 
 function majorityElement(nums: number[]): number {
+  const map: Record<number, number> = {};
+
+  nums.forEach((num) => {
+    if (map[num]) {
+      map[num]++;
+    } else {
+      map[num] = 1;
+    }
+  });
+
+  for (const key in map) {
+    const x = nums.length / 2;
+    if (map[key] > x) {
+      return Number(key);
+    }
+  }
+
   return 0;
 }
 
 console.log(majorityElement([3, 2, 3]));
 console.log(majorityElement([2, 2, 1, 1, 1, 2, 2]));
+
+// takes in an array of number
+// returns majority item - the item that appears more than half the length of the array
+// example: majorityElement([3,2,3]) => 3 because 3 appears twice which is the more than half the length of the array. 2 > 3/2
+// find the item that appears the most
