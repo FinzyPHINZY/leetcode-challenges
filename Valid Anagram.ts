c07b3d174480ff8a65b6fa806bc2b46440f415a3
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2024-11-17 15:33:13 +0100

    added solution to problem

--- Valid Anagram.ts
+++ Valid Anagram.ts
@@ -20,8 +20,14 @@
 //     s and t consist of lowercase English letters.
 
 function isAnagram(s: string, t: string): boolean {
-  return false;
+  const sortedS = s.split('').sort().join('');
+  const sortedT = t.split('').sort().join('');
+  return sortedS === sortedT;
 }
 
 console.log(isAnagram('anagram', 'nagaram'));
 console.log(isAnagram('rat', 'car'));
+
+// takes in two strings
+// returns a boolean value indicating whether the first string is a valid anagram of the other strng
+// sort both strings and compare them.
