f1ac1d4369a19663860f27e3ae8886fd94c9c582
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2024-11-20 09:02:12 +0100

    added test cases

--- Move Zeroes.ts
+++ Move Zeroes.ts
@@ -23,3 +23,6 @@
  Do not return anything, modify nums in-place instead.
  */
 function moveZeroes(nums: number[]): void {}
+
+console.log(moveZeroes([0, 1, 0, 3, 12]));
+console.log(moveZeroes([0]));
