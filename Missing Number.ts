722100df9f457a1d06322a9b211122911686325e
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2024-11-20 09:52:37 +0100

    added starter code and test cases

--- Missing Number.ts
+++ Missing Number.ts
@@ -26,3 +26,11 @@
 //     All the numbers of nums are unique.
 
 // Follow up: Could you implement a solution using only O(1) extra space complexity and O(n) runtime complexity?
+
+function missingNumber(nums: number[]): number {
+  return 0;
+}
+
+console.log(missingNumber([3, 0, 1]));
+console.log(missingNumber([0, 1]));
+console.log(missingNumber([9, 6, 4, 2, 3, 5, 7, 0, 1]));
