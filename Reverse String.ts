d438034ecf00c3a183e8b841b9b2b62cf2bf74f8
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2024-11-22 09:29:09 +0100

    added solution to problem

--- Reverse String.ts
+++ Reverse String.ts
@@ -16,3 +16,14 @@
 
 //     1 <= s.length <= 105
 //     s[i] is a printable ascii character.
+
+/**
+ Do not return anything, modify s in-place instead.
+ */
+function reverseString(s: string[]): void {
+  s = s.reverse();
+  console.log(s);
+}
+
+console.log(reverseString(['h', 'e', 'l', 'l', 'o']));
+console.log(reverseString(['H', 'a', 'n', 'n', 'a', 'h']));
