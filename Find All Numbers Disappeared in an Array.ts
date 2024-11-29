70a4f0fbf7baa57ab3253e2c7b72cffafc637a40
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2024-11-29 12:55:34 +0100

    added test cases

--- Find All Numbers Disappeared in an Array.ts
+++ Find All Numbers Disappeared in an Array.ts
@@ -21,3 +21,6 @@
 function findDisappearedNumbers(nums: number[]): number[] {
   return [0];
 }
+
+console.log(findDisappearedNumbers([4, 3, 2, 7, 8, 2, 3, 1]));
+console.log(findDisappearedNumbers([1, 1]));
