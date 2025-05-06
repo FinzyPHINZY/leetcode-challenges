05d38f2e737fcb6e7fb8d452e2049d694a4edf23
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2025-05-06 10:03:09 +0100

    added new practice and solution

--- practice.ts
+++ practice.ts
@@ -70,6 +70,73 @@ function romanToIntP(roman: string): number {
   return result;
 }
 
-console.log(romanToIntP('III')); // Output: 3
-console.log(romanToIntP('LVIII')); // Output: 58
-console.log(romanToIntP('MCMXCIV')); // Output: 1994
+// console.log(romanToIntP('III')); // Output: 3
+// console.log(romanToIntP('LVIII')); // Output: 58
+// console.log(romanToIntP('MCMXCIV')); // Output: 1994
+
+// Given a string containing digits from 2-9 inclusive, return all possible letter combinations that the number could represent. Return the answer in any order.
+
+// A mapping of digits to letters (just like on the telephone buttons) is given below. Note that 1 does not map to any letters.
+
+//   const map: { [key: string]: string[] } = {
+//     '2': ['a', 'b', 'c'],
+//     '3': ['d', 'e', 'f'],
+//     '4': ['g', 'h', 'i'],
+//     '5': ['j', 'k', 'l'],
+//     '6': ['m', 'n', 'o'],
+//     '7': ['p', 'q', 'r', 's'],
+//     '8': ['t', 'u', 'v'],
+//     '9': ['w', 'x', 'y', 'z'],
+//   }
+
+// Example 1:
+
+// Input: digits = "23"
+// Output: ["ad","ae","af","bd","be","bf","cd","ce","cf"]
+
+// Example 2:
+
+// Input: digits = ""
+// Output: []
+
+// Example 3:
+
+// Input: digits = "2"
+// Output: ["a","b","c"]
+
+function letterCombinationsP(str: string): string[] {
+  const map: { [key: string]: string[] } = {
+    '2': ['a', 'b', 'c'],
+    '3': ['d', 'e', 'f'],
+    '4': ['g', 'h', 'i'],
+    '5': ['j', 'k', 'l'],
+    '6': ['m', 'n', 'o'],
+    '7': ['p', 'q', 'r', 's'],
+    '8': ['t', 'u', 'v'],
+    '9': ['w', 'x', 'y', 'z'],
+  };
+
+  const result: string[] = [];
+
+  const backtrack = (combination: string, nextDigits: string) => {
+    if (nextDigits.length === 0) {
+      result.push(combination);
+    } else {
+      const digit = nextDigits[0];
+      const letters = map[digit];
+      if (letters) {
+        for (const letter of letters) {
+          backtrack(combination + letter, nextDigits.slice(1));
+        }
+      }
+    }
+  };
+
+  backtrack('', str);
+
+  return result;
+}
+
+console.log(letterCombinationsP('23'));
+// console.log(letterCombinationsP(''));
+// console.log(letterCombinationsP('2'));
