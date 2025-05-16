d0c6925c3a664858c126144371276756f6d58ddd
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2025-05-16 04:20:55 +0100

    added new practice and solution

--- practice.ts
+++ practice.ts
@@ -242,5 +242,63 @@ function removeDuplicatesP(nums: number[]): number {
   return [...set].length;
 }
 
-console.log(removeDuplicatesP([1, 1, 2])); // Output: 2
-console.log(removeDuplicatesP([0, 0, 1, 1, 1, 2, 2, 3, 3, 4])); // Output: 5
+// console.log(removeDuplicatesP([1, 1, 2])); // Output: 2
+// console.log(removeDuplicatesP([0, 0, 1, 1, 1, 2, 2, 3, 3, 4])); // Output: 5
+
+// Given a string s, find the length of the longest
+// substring
+// without repeating characters.
+
+// Example 1:
+
+// Input: s = "abcabcbb"
+// Output: 3
+// Explanation: The answer is "abc", with the length of 3.
+
+// Example 2:
+
+// Input: s = "bbbbb"
+// Output: 1
+// Explanation: The answer is "b", with the length of 1.
+
+// Example 3:
+
+// Input: s = "pwwkew"
+// Output: 3
+// Explanation: The answer is "wke", with the length of 3.
+// Notice that the answer must be a substring, "pwke" is a subsequence and not a substring.
+
+// Constraints:
+
+//     0 <= s.length <= 5 * 104
+//     s consists of English letters, digits, symbols and spaces.
+
+function lengthOfLongestSubstringP(str: string): number {
+  if (str.length < 2) return str.length;
+
+  const subStrings = [];
+
+  let subStr = '';
+
+  for (let i = 0; i < str.length; i++) {
+    const char = str[i];
+
+    if (!subStr.includes(char)) {
+      subStr += char;
+    } else {
+      subStrings.push(subStr);
+      subStr = char;
+    }
+  }
+
+  const sorted = subStrings.sort((a, b) => b.length - a.length);
+
+  return sorted[0].length;
+}
+
+console.log(lengthOfLongestSubstringP('abcabcbb')); // Output: 3
+console.log(lengthOfLongestSubstringP('bbbbb')); // Output: 1
+console.log(lengthOfLongestSubstringP('pwwkew')); // Output: 3
+console.log(lengthOfLongestSubstringP('')); // Output: 0
+console.log(lengthOfLongestSubstringP(' ')); // Output: 1
+console.log(lengthOfLongestSubstringP('a')); // Output: 1
