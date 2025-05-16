468acdff6da2a1616e7b670eaf358faac909f50c
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2025-05-16 04:38:26 +0100

    added new practice and solution

--- practice.ts
+++ practice.ts
@@ -296,9 +296,64 @@ function lengthOfLongestSubstringP(str: string): number {
   return sorted[0].length;
 }
 
-console.log(lengthOfLongestSubstringP('abcabcbb')); // Output: 3
-console.log(lengthOfLongestSubstringP('bbbbb')); // Output: 1
-console.log(lengthOfLongestSubstringP('pwwkew')); // Output: 3
-console.log(lengthOfLongestSubstringP('')); // Output: 0
-console.log(lengthOfLongestSubstringP(' ')); // Output: 1
-console.log(lengthOfLongestSubstringP('a')); // Output: 1
+// console.log(lengthOfLongestSubstringP('abcabcbb')); // Output: 3
+// console.log(lengthOfLongestSubstringP('bbbbb')); // Output: 1
+// console.log(lengthOfLongestSubstringP('pwwkew')); // Output: 3
+// console.log(lengthOfLongestSubstringP('')); // Output: 0
+// console.log(lengthOfLongestSubstringP(' ')); // Output: 1
+// console.log(lengthOfLongestSubstringP('a')); // Output: 1
+
+// Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
+// You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+// You can return the answer in any order.
+
+// Example 1:
+
+// Input: nums = [2,7,11,15], target = 9
+// Output: [0,1]
+// Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+
+// Example 2:
+
+// Input: nums = [3,2,4], target = 6
+// Output: [1,2]
+
+// Example 3:
+
+// Input: nums = [3,3], target = 6
+// Output: [0,1]
+
+// Constraints:
+
+//     2 <= nums.length <= 104
+//     -109 <= nums[i] <= 109
+//     -109 <= target <= 109
+//     Only one valid answer exists.
+
+function twoSumP(arr: number[], num: number) {
+  // iterate through arr.
+  // check for the sum of two numbers.
+  // return the index of the two numbers.
+
+  if (arr.length < 2) return [];
+
+  if (arr.length === 2) {
+    if (arr[0] + arr[1] === num) return [0, 1];
+  }
+
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr.length; j++) {
+      if (arr[i] === arr[j]) continue;
+
+      if (arr[i] + arr[j] === num) {
+        return [i, j];
+      }
+    }
+  }
+}
+
+console.log(twoSumP([2, 7, 11, 15], 9)); // Output: [0,1]
+console.log(twoSumP([3, 2, 4], 6)); // Output: [1,2]
+console.log(twoSumP([3, 3], 6)); // Output: [0,1]
